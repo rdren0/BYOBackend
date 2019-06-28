785fa2144233f6be538fb7dae3877f6975035fd7
Rachael Drennan <rdrennan0@gmail.com>
2019-06-28 09:39:10 -0600

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,21 @@
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/deargod',
+    client: "pg",
+    connection: "postgres://localhost/deargod",
     migrations: {
-      directory: './db/migrations'
+      directory: "./db/migrations"
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: "./db/seeds/dev"
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: "./db/migrations"
     },
     useNullAsDefault: true
   }
-},
-production: {
-  client: 'pg',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
-  migrations: {
-    directory: './db/migrations'
-  },
-  useNullAsDefault: true
 };
