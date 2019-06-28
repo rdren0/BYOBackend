8f5f9e96afcae16c9b03f41ea56f16b2ba973b1c
Rachael Drennan <rdrennan0@gmail.com>
2019-06-28 09:29:59 -0600

    update files

---
+++ server.js
@@ -0,0 +1,48 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const express = require('express');
+const app = express()
+const port = 3000;
+
+app.use(express.json());
+app.listen(3000, () => {
+    console.log(`App is running 🏃 on port ${port}`)
+})
+
+app.get('/api/v1/parks', (request, response) => {
+    database('parks').select()
+      .then((parks) => {
+        response.status(200).json(parks);
+      })
+      .catch((error) => {
+        response.status(500).json({ error });
+      });
+      return 'Hello world';
+  });
+
+//   app.get('/api/v1/papers', (request, response) => {
+//     database('footnotes').select()
+//       .then((footnotes) => {
+//         response.status(200).json(footnotes);
+//       })
+//       .catch((error) => {
+//         response.status(500).json({ error });
+//       });
+//   });
+
+//   app.post('/api/v1/papers', (request, response) => {
+//     const newPaper = request.body;
+//       if (!newPaper.title && !newPaper.author) {
+//         return response
+//           .status(422)
+//           .send({ error: `Expected { title: <String>, author: <String> }. You're missing info. you sent ${newPaper.title}` });
+//       }
+//     database('papers').insert(newPaper, 'id')
+//       .then(paper => {
+//         response.status(201).json({ id: paper[0] })
+//       })
+//       .catch(error => {
+//         response.status(500).json({ error });
+//       });
+//   });
