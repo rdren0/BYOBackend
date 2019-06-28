f806e208db0faec717927ef4b1b6aca18bd2ef16
Rachael Drennan <rdrennan0@gmail.com>
2019-06-28 10:02:40 -0600

    update server import oder

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+const express = require('express');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-const express = require('express');
 const app = express()
-app.set('port', process.env.PORT || 3000).
+const PORT =  process.env.PORT || 3000;
 
 app.use(express.json());
-app.listen(process.env.PORT || 3000, function(){
+app.listen(PORT, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
@@ -21,24 +21,24 @@ app.get('/api/v1/parks', (request, response) => {
       console.log('Hello world') ;
   });
 
-//   app.get('/api/v1/papers', (request, response) => {
-//     database('footnotes').select()
-//       .then((footnotes) => {
-//         response.status(200).json(footnotes);
+//   app.get('/api/v1/states', (request, response) => {
+//     database('parks').select()
+//       .then((parks) => {
+//         response.status(200).json(parks);
 //       })
 //       .catch((error) => {
 //         response.status(500).json({ error });
 //       });
 //   });
 
-//   app.post('/api/v1/papers', (request, response) => {
+//   app.post('/api/v1/states', (request, response) => {
 //     const newPaper = request.body;
 //       if (!newPaper.title && !newPaper.author) {
 //         return response
 //           .status(422)
 //           .send({ error: `Expected { title: <String>, author: <String> }. You're missing info. you sent ${newPaper.title}` });
 //       }
-//     database('papers').insert(newPaper, 'id')
+//     database('states').insert(newPaper, 'id')
 //       .then(paper => {
 //         response.status(201).json({ id: paper[0] })
 //       })
